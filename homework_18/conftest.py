07eb609f038759e39fbf636071b52cffac0c8674
KiraGol <kiragolovkova93@gmail.com>
2021-09-18 16:10:17 +0300

    pages

---
+++ homework_18/conftest.py
@@ -0,0 +1,22 @@
+import time
+
+from selenium.webdriver import Chrome
+import pytest
+
+from homework_18.pages.dashboard import Dashboard
+
+
+@pytest.fixture(scope="session")
+def driver() -> Chrome:
+    driver = Chrome("../driver/chromedriver")
+    driver.maximize_window()
+    driver.get("https://qwertyshop.ua/")
+    time.sleep(3)
+    yield driver
+
+    driver.quit()
+
+
+@pytest.fixture
+def dashboard(driver) -> Dashboard:
+    yield Dashboard(driver)
